3b6a36cd608bbc72e326a224693ed4c5bcbfd58a
natan20200679 <natanaelmedrado@outlook.com>
2021-03-23 20:28:17 -0300

    Atualização Lista de Compreensões

--- Basico/Operadores Basicos/Operadores Aritmeticos (2).py
+++ Basico/Operadores Basicos/Operadores Aritmeticos (2).py
@@ -1,6 +1,15 @@
 if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
+    a = int(input('Valor de a?'))
+    b = int(input('valor de b?'))
+    print(a + b)
+    print(a - b)
+    print(a * b)
+
+
+
+
+
+
 
 
 
