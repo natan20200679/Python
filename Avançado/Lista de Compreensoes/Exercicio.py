c95155edb7f82f959f3246d998b37b7f1615db1c
natan20200679 <natanaelmedrado@outlook.com>
2021-03-22 22:33:46 -0300

    Estudo do Python Avançado (List Comprehension)

---
+++ Avançado/Lista de Compreensoes/Exercicio.py
@@ -0,0 +1,8 @@
+numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+newlist = []
+
+def int(x):
+    for x in numbers:
+        if x > 0:
+            newlist.append(x,numbers)
+    print(newlist)
